40169040dcb9b441be3e5195d6dea3431f46f7d5
vinay baswa <vinaybaswa@gmail.com>
2020-09-28 21:53:56 -0400

    add state to App (Lifting State Up)

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "components/Application.scss";
-import DayList from "components/DayList.js";
+import React, { useState } from "react";
+import "./Application.scss";
+import DayList from "./DayList";
 
 const days = [
   {
@@ -21,27 +21,28 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-    days={days}
-    day={"Monday"}
-    setDay={day => console.log(day)}
-  />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
