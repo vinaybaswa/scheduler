120f7bfa365da48c813ddb7af18b097ef6fa16dd
vinay baswa <vinaybaswa@gmail.com>
2020-09-30 09:54:21 -0400

    build Status component

--- stories/index.js
+++ stories/index.js
@@ -16,8 +16,9 @@ import Empty                  from "../src/components/Appointment/Empty"
 import Show                   from "../src/components/Appointment/Show"
 import Confirm                from "../src/components/Appointment/Confirm"
 import Status                 from "../src/components/Appointment/Status"
+import Error                  from "../src/components/Appointment/Error"
 
-      //BUTTON//
+//BUTTON//
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -34,7 +35,7 @@ storiesOf("Button", module)
     </Button>
   ));
 
-      //DAY-LIST-ITEM//
+//DAY-LIST-ITEM//
 storiesOf("DayListItem", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -47,7 +48,7 @@ storiesOf("DayListItem", module)
   ));
 
 
-      //DAY-LIST//
+//DAY-LIST//
 const days = [
   {
     id: 1,
@@ -77,8 +78,7 @@ storiesOf("DayList", module)
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
   ));
 
-
-        //INTERVIEWER-LIST-ITEM//
+//INTERVIEWER-LIST-ITEM//
 const interviewer = {
   id: 1,
   name: "Sylvia Palmer",
@@ -113,34 +113,34 @@ storiesOf("InterviewerListItem", module)
     />
   ));
 
-          //INTERVIEWER-LIST-ITEM//
-  const interviewers = [
-    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  ];
-  
-  storiesOf("InterviewerList", module)
-    .addParameters({
-      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-    })
-    .add("Initial", () => (
-      <InterviewerList
-        interviewers={interviewers}
-        setInterviewer={action("setInterviewer")}
-      />
-    ))
-    .add("Preselected", () => (
-      <InterviewerList
-        interviewers={interviewers}
-        interviewer={3}
-        setInterviewer={action("setInterviewer")}
-      />
-    ));
+//INTERVIEWER-LIST-ITEM//
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 
-            //APPOINTMENT//
+storiesOf("InterviewerList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      setInterviewer={action("setInterviewer")}
+    />
+  ))
+  .add("Preselected", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      interviewer={3}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
+
+//APPOINTMENT//
 storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
@@ -165,3 +165,9 @@ storiesOf("Appointment", module)
     />
   ))
   .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  ))
