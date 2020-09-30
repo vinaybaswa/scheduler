b261afaee273803206c8da5d7882c07a28e3cf9d
vinay baswa <vinaybaswa@gmail.com>
2020-09-29 23:49:57 -0400

    build Show component

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList        from "components/InterviewerList"
 import Appointment            from "../src/components/Appointment/index"
 import Header                 from "../src/components/Appointment/Header"
 import Empty                  from "../src/components/Appointment/Empty"
+import Show                  from "../src/components/Appointment/Show"
 
       //BUTTON//
 storiesOf("Button", module)
@@ -139,10 +140,18 @@ storiesOf("InterviewerListItem", module)
 
             //APPOINTMENT//
 storiesOf("Appointment", module)
-.addParameters({
-  backgrounds: [{ name: "white", value: "#fff", default: true }]
-})
-.add("Appointment", () => <Appointment />)
-.add("Appointment with Time", () => <Appointment time="12pm" />)
-.add("Header", () => <Header time="12pm" />)
-.add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show
+      studentName="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}  
+    />
+  ));
