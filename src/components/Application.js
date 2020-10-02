2856fdf08fc2d69774473686e32d1726d848f1ea
vinay baswa <vinaybaswa@gmail.com>
2020-10-01 21:28:41 -0400

    install axios and fetch days with api request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment"
@@ -51,28 +52,18 @@ const appointments = [
   
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const slot = appointments.map((appointment, index) => {
+  useEffect(() => {
+    const daysURL = `http://localhost:8001/api/days`;
+    axios.get(daysURL).then(response => {
+      setDays(response.data)
+    });
+  },[])
+
+  const appointment = appointments.map((appointment, index) => {
     return (
       <Appointment key={index} {...appointment} />
     );
@@ -101,7 +92,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {slot}
+        {appointment}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
